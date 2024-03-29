54d8acab02edcfe01e9948e946c1d95343474931
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-03-29 12:28:36 -0500

    resampling

--- src/make_subrois.sh
+++ src/make_subrois.sh
@@ -6,6 +6,10 @@
 
 # FIXME need to limit to single coronal slice
 
+subj_dir=SUBJECT
+seg=${subj_dir}/mri/lh.hippoAmygLabels.mgz
+
+
 # Hippocampal head
 #    203 parasubiculum
 #   233  presubiculum-head
@@ -15,34 +19,50 @@
 #   241  CA4-head
 #   243  GC-ML-DG-head
 #   245  molecular_layer_HP-head
-mri_binarize --i rr_lh.hippoAmygLabels.mgz --o hipphead.mgz \
+mri_binarize --i ${seg} --o hipphead.mgz \
     --match 203 233 235 237 239 241 243 245
-
-# Posterior-most extent of hippocampal head
-ext_head=($(compute_extents.py hipphead.mgz))
-head_posterior_coord=${ext_head[2]}
-
-# Move back 2mm more
-coronal_coord=$((head_posterior_coord-2))
-
-# Now we either need to resample so we have a true coronal slice,
-# then use compute_extents.py again; or, have some way of finding 
-# the subicular and DG extents at the coronal_coord exactly
-
-# Resampling is easy but what target vol should we use? Do we need
-# to retain the high spatial resolution?
-
 
 # subicular complex
 #   234  presubiculum-body
 #   236  subiculum-body
 #   238  CA1-body
-mri_binarize --i rr_lh.hippoAmygLabels.mgz --o subicular.mgz \
+mri_binarize --i ${seg} --o subicular.mgz \
     --match 234 236 238
 
 # dentate gyrus
 #   242  CA4-body
 #   244  GC-ML-DG-body
 #   246  molecular_layer_HP-body
-mri_binarize --i rr_lh.hippoAmygLabels.mgz --o dentate.mgz \
+mri_binarize --i ${seg} --o dentate.mgz \
     --match 242 244 246
+
+# Transform to Tal space without resampling
+for img in hipphead.mgz subicular.mgz dentate.mgz; do
+    mri_vol2vol \
+        --mov ${img} \
+        --targ "${FREESURFER_HOME}"/subjects/cvs_avg35_inMNI152/mri/orig/001.mgz \
+        --xfm ${subj_dir}/mri/transforms/talairach.xfm \
+        --no-resample \
+        --o r${img}
+done
+
+# Rotate to align long axis with Y, resampling this time. This reduces
+# spatial resolution to whatever the atlas is.
+rotdeg=-40
+for img in rhipphead.mgz rsubicular.mgz rdentate.mgz; do
+    mri_vol2vol \
+        --mov ${img} \
+        --targ "${FREESURFER_HOME}"/subjects/cvs_avg35_inMNI152/mri/orig/001.mgz  \
+        --regheader \
+        --rot ${rotdeg} 0 0 \
+        --nearest \
+        --o r${img}
+done
+
+# Posterior-most extent of hippocampal head
+ext_head=($(compute_extents.py hipphead.mgz))
+head_posterior_coord=${ext_head[2]}
+
+# Move back 2mm more and find a 5mm slice
+coronal_max=$((head_posterior_coord-2))
+coronal_min=$((head_posterior_coord-5))
