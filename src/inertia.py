f6c3550761cfcc156ab2ddf914a94df8b82a5304
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-04-01 14:58:23 -0500

    working

--- src/inertia.py
+++ src/inertia.py
@@ -3,25 +3,40 @@
 import argparse
 import nibabel
 import numpy
-from scipy.spatial.transform import Rotation
+import os
+
+def get_hipp_xyz(img):
+    data = img.get_fdata()
+    idx = numpy.where((data>0) & (data<1000))  # find hippocampus voxels
+    ijk = numpy.vstack(idx).T  # list of arrays to (voxels, 3) array
+    xyz = nibabel.affines.apply_affine(img.affine, ijk)  # get mm coords
+    return xyz
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--mask_niigz', required=True)
+parser.add_argument('--img_dir', required=True)
 args = parser.parse_args()
 
-img = nibabel.load(args.mask_niigz)
+lh_img = nibabel.load(os.path.join(args.img_dir,'lh.hippoAmygLabels.nii.gz'))
+rh_img = nibabel.load(os.path.join(args.img_dir,'rh.hippoAmygLabels.nii.gz'))
 
-data = img.get_fdata()
+lh_xyz = get_hipp_xyz(lh_img)
+rh_xyz = get_hipp_xyz(rh_img)
 
-idx = numpy.where(data)  # find voxels that are not zeroes
-ijk = numpy.vstack(idx).T  # list of arrays to (voxels, 3) array
-xyz = nibabel.affines.apply_affine(img.affine, ijk)  # get mm coords
+# mm coords of all voxels in both hippocampi
+xyz = numpy.concatenate((lh_xyz, rh_xyz), axis=0)
 
-# Coords relative to center of mass
-x = xyz[:,0] - numpy.mean(xyz[:,0])
-y = xyz[:,1] - numpy.mean(xyz[:,1])
-z = xyz[:,2] - numpy.mean(xyz[:,2])
+# mm coords relative to center of mass
+com = numpy.array([
+    numpy.mean(xyz[:,0]),
+    numpy.mean(xyz[:,1]),
+    numpy.mean(xyz[:,2]),
+    ])
 
+x = xyz[:,0] - com[0]
+y = xyz[:,1] - com[1]
+z = xyz[:,2] - com[2]
+
+# Inertia tensor
 Ixx = numpy.sum(numpy.square(y) + numpy.square(z))
 Iyy = numpy.sum(numpy.square(x) + numpy.square(z))
 Izz = numpy.sum(numpy.square(x) + numpy.square(y))
@@ -30,20 +45,64 @@ Ixy = -numpy.sum(numpy.multiply(x, y))
 Ixz = -numpy.sum(numpy.multiply(x, z))
 Iyz = -numpy.sum(numpy.multiply(y, z))
 
-# Inertial tensor
 Imat = numpy.array([
     [Ixx, Ixy, Ixz],
     [Ixy, Iyy, Iyz],
     [Ixz, Iyz, Izz],
     ])
 
-# SVD of the inertial tensor gives a rotation matrix
-# Ue (=Ve.T) to the principal axes
+# SVD of the inertial tensor gives a rotation matrix Ue 
+# (=Ve.T) to the principal axes
 Ue, Se, Ve = numpy.linalg.svd(Imat)
 
-# Use scipy Rotation class to convert to angles.
-# No idea what order mri_vol2vol applies its rotations in.
-r = Rotation.from_matrix(Ue)
-rots = r.as_euler('xyz', degrees=True)
-print(f'{rots[0]} {rots[1]} {rots[2]}')
+# But we need to re-sort axes and transpose to get the right result.
+# What is the principled way to do this?
+# As this uses xyz mm coords, the data order of the file is irrelevant.
+t = numpy.array([
+    [0, 0, 1],
+    [0, 1, 0],
+    [-1, 0, 0],
+])
+reUe = numpy.matmul(Ue,t).T
 
+#testmat = numpy.array([
+#    [1, 0, 0],
+#    [0, .9397, -.3420],
+#    [0, .3420, .9397],
+#])
+#print('testmat'), print(testmat)
+
+# Translate COM to origin
+# Rotate
+# Translate COM back to position
+transmat0 = numpy.array([
+    [1, 0, 0, -com[0]],
+    [0, 1, 0, -com[1]],
+    [0, 0, 1, -com[2]],
+    [0, 0, 0, 1],
+    ])
+transmatCOM = numpy.array([
+    [1, 0, 0, com[0]],
+    [0, 1, 0, com[1]],
+    [0, 0, 1, com[2]],
+    [0, 0, 0, 1],
+    ])
+
+# Apply the rotation to the affines and save rotated images.
+# We first translate to COM, then rotate, then translate back,
+# and the resulting matrix is left side multiplied with the affine
+# to produce the new affine.
+rotmat = reUe;
+rotmat = numpy.hstack(( rotmat, numpy.array([[0], [0], [0]]) ))
+rotmat = numpy.vstack(( rotmat, numpy.array([[0, 0, 0, 1]]) ))
+
+allmat = numpy.matmul(rotmat, transmat0)
+allmat = numpy.matmul(transmatCOM, allmat)
+
+new_lh_affine = numpy.matmul(allmat, lh_img.affine)
+new_lh_img = nibabel.Nifti1Image(lh_img.get_fdata(), new_lh_affine)
+nibabel.save(new_lh_img, os.path.join(args.img_dir, 'rlh.hippoAmygLabels.nii.gz'))
+
+new_rh_affine = numpy.matmul(allmat, rh_img.affine)
+new_rh_img = nibabel.Nifti1Image(rh_img.get_fdata(), new_rh_affine)
+nibabel.save(new_rh_img, os.path.join(args.img_dir, 'rrh.hippoAmygLabels.nii.gz'))
--- src/prep_inertia.sh
+++ src/prep_inertia.sh
@@ -1,109 +1,25 @@
 #!/usr/bin/env bash
 #
-# Rotate principal inertial axes of bilateral hippocampus ROI
-# to align with XYZ axes in image mm space.
-#
-# Working, but FIXME how to apply to the actual segmentations?
+# subj_dir
+# out_dir
 
+# Work in output directory
+cd "${out_dir}"
+
+# Convert hippocampal segs to nifti for nibabel
 for hemi in lh rh; do
-
-    # Threshold to hippocampus only
-    mri_binarize \
-        --i "${subj_dir}"/mri/${hemi}.hippoAmygLabels.mgz \
-        --min 1 --max 1000 \
-        --o ${hemi}.hippomask.nii.gz
-
-    # Resample to same grid. This is needed to combine lh/rh,
-    # plus we will want them in register with some version of
-    # nu image for viewing
-    mri_vol2vol \
-        --mov ${hemi}.hippomask.nii.gz \
-        --targ "${subj_dir}"/mri/nu.mgz \
-        --regheader \
-        --nearest \
-        --o ${hemi}.hippomask.nii.gz
-
-    # Convert full seg to nifti
     mri_convert \
         "${subj_dir}"/mri/${hemi}.hippoAmygLabels.mgz \
         ${hemi}.hippoAmygLabels.nii.gz
-    
 done
 
+# Rotate to align principal axes of hippocampus with image axes
+inertia.py --img_dir "${out_dir}"
 
-# Combine hemispheres
-mri_concat \
-    --i lh.hippomask.nii.gz rh.hippomask.nii.gz \
-    --o hippomasks.nii.gz \
-    --sum
-
-
-## So far, all images are aligned in fsleyes and freeview and
-# show "Orientation : LIA" by mri_info but with differing FOV,
-# voxel size, and vox2ras transforms.
-
-
-# Test a 90 degree rotation without resampling. Doesn't work -
-# I bet the rotation is happening around different centers based
-# on FOVs - corner or center voxel. Orientation after rotation 
-# looks the same, but position is different.
-mri_vol2vol \
-    --mov lh.hippomask.nii.gz \
-    --targ lh.hippomask.nii.gz \
-    --regheader \
-    --rot 90 0 0 \
-    --no-resample \
-    --o rlh.hippomask.nii.gz
-
-mri_vol2vol \
-    --mov lh.hippoAmygLabels.nii.gz \
-    --targ lh.hippoAmygLabels.nii.gz \
-    --regheader \
-    --rot 90 0 0 \
-    --no-resample \
-    --o rlh.hippoAmygLabels.nii.gz
-
-
-
-# Get mask into RAS (not actually needed - same result without)
-#mri_convert \
-#    --out_orientation RAS \
-#    hippomasks.nii.gz \
-#    hippomasks.nii.gz
-
-# Estimate and apply rotations
-rots=$(inertia.py --mask_niigz hippomasks.nii.gz)
-mri_vol2vol \
-    --mov hippomasks.nii.gz \
-    --targ hippomasks.nii.gz \
-    --regheader \
-    --rot $rots \
-    --no-resample \
-    --o rhippomasks.nii.gz
-
-mri_vol2vol \
-    --mov rhippomasks.nii.gz \
-    --targ hippomasks.nii.gz \
-    --regheader \
-    --rot 0 90 0 \
-    --nearest \
-    --o rhippomasks.nii.gz
-
-
-# Apply to hi res segs. Can't apply rots directly
-# because accuracy is dependent on FOV
-mri_vol2vol \
-    --mov lh.hippoAmygLabels.nii.gz \
-    --targ lh.hippoAmygLabels.nii.gz \
-    --regheader \
-    --rot $rots \
-    --no-resample \
-    --o rot-lh.hippoAmygLabels.nii.gz
-
-mri_vol2vol \
-    --mov rot-lh.hippoAmygLabels.nii.gz \
-    --targ rot-lh.hippoAmygLabels.nii.gz \
-    --regheader \
-    --rot 0 90 0 \
-    --no-resample \
-    --o rot-lh.hippoAmygLabels.nii.gz
+# Compute IHI metrics
+for hemi in lh rh; do
+    compute_ihi.py \
+        --seg_niigz r${hemi}.hippoAmygLabels.nii.gz \
+        --out_dir "${out_dir}" \
+        > r${hemi}.hippoAmygLabels-info.txt
+done
